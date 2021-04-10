a38ef5fe9bc3b90721a2857742fb32d7feef26fe
xuzhihang@utopa.com.cn <xuzhihang@utopa.com.cn>
2021-04-10 10:29:14 +0800

    回文链表

---
+++ 234.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function (head) {
+    let left = head;
+    function traverse(right) {
+        if (right == null) return true;
+        let res = traverse(right.next);
+        res = res && (right.val === left.val);
+        left = left.next;
+        return res;
+    }
+    return traverse(head);
+};
+
+function ListNode(val, next = null) {
+  this.val = val;
+  this.next = next;
+}
+
+const getListFromArray = (a) => {
+  let dummy = new ListNode();
+  let pre = dummy;
+  a.forEach((x) => (pre = pre.next = new ListNode(x)));
+  return dummy.next;
+};
+
+// console.log(isPalindrome(getListFromArray([1])));
+console.log(isPalindrome(getListFromArray([1, 0, 1])));
